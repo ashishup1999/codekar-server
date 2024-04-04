4c61adfb3036c2151855df081e698de9042e341c
Ashish Upadhyay <ashishup1999@gmail.com>
2024-04-04 11:51:11 +0530

    TLE and OLE added for compiler

--- app/handlers/CompileHandler.go
+++ app/handlers/CompileHandler.go
@@ -3,7 +3,9 @@ package handlers
 import (
 	"codekar/app/models"
 	"codekar/app/services"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,30 +19,55 @@ const (
 )
 
 func CompileCode(c *gin.Context) {
-	var req models.CompileReq
-	if err := c.BindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
+	//timeout creation and task channel creation
+	timeout := time.After(10 * time.Second)
+	taskExecuted := make(chan string)
+
+	// Simulate a long-running task
+	go func() {
+		time.Sleep(3 * time.Second)
+	}()
+	go func() {
+		//throw data to channel
+		defer func() {
+			taskExecuted <- "executed"
+		}()
+		var req models.CompileReq
+		if err := c.BindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		var output string
+		var err error
+		if req.Language == JAVA {
+			output, err = services.RunJavafn(req)
+		} else if req.Language == PYTHON {
+			output, err = services.RunPythonfn(req)
+		} else if req.Language == CPP {
+			output, err = services.RunCppfn(req)
+		} else if req.Language == GO {
+			output, err = services.RunGofn(req)
+		} else if req.Language == JS {
+			output, err = services.RunJSfn(req)
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Language not supported"})
+			return
+		}
+		if err != nil {
+			c.JSON(500, models.CompileResp{Status: "ERROR", Message: "Execution Failed", Output: err.Error()})
+			return
+		} else if len(output) > 10000 {
+			c.JSON(http.StatusAccepted, models.CompileResp{Status: "SUCCESS", Message: "OLE", Output: "Output Limit Exceded"})
+			return
+		}
+		c.JSON(http.StatusAccepted, models.CompileResp{Status: "SUCCESS", Message: "Compiled Successfuly", Output: output})
+	}()
+
+	// Wait for either the task to complete or the timeout to expire
+	select {
+	case <-timeout:
+		c.JSON(http.StatusAccepted, models.CompileResp{Status: "SUCCESS", Message: "TLE", Output: "Time Limit Exceeded"})
+	case t := <-taskExecuted:
+		fmt.Println(t)
 	}
-	var output string
-	var err error
-	if req.Language == JAVA {
-		output, err = services.RunJavafn(req)
-	} else if req.Language == PYTHON {
-		output, err = services.RunPythonfn(req)
-	} else if req.Language == CPP {
-		output, err = services.RunCppfn(req)
-	} else if req.Language == GO {
-		output, err = services.RunGofn(req)
-	} else if req.Language == JS {
-		output, err = services.RunJSfn(req)
-	} else {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Language not supported"})
-		return
-	}
-	if err != nil {
-		c.JSON(500, models.CompileResp{Status: "ERROR", Message: "Execution Failed", Output: err.Error()})
-		return
-	}
-	c.JSON(http.StatusAccepted, models.CompileResp{Status: "SUCCESS", Message: "Compiled Successfuly", Output: output})
 }
